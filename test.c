eda4bd582ee916cc232b8366a7e54d573b0fda88
Thomas Munro <thomas.munro@enterprisedb.com>
2018-11-21 16:47:37 +1300

    hack

--- test.c
+++ test.c
@@ -5,15 +5,8 @@
 int
 main(int argc, char *argv[])
 {
-	printf("Creating directory my_dir...\n");
-	if (!CreateDirectoryA("my_dir", NULL))
-	{
-		printf("Failed, error = %d, errno = %d (%s).\n", GetLastError(), errno, strerror(errno));
-		exit(1);
-	}
-
-	printf("Creating file my_dir\\foo.txt...\n");
-	HANDLE file = CreateFile("my_dir\\foo.txt", GENERIC_WRITE,
+	printf("Creating file foo.txt...\n");
+	HANDLE file = CreateFile("foo.txt", GENERIC_WRITE,
 							 FILE_SHARE_DELETE, NULL,
 							 CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (file == INVALID_HANDLE_VALUE)
@@ -22,31 +15,23 @@ main(int argc, char *argv[])
 		exit(1);
 	}
 
-	printf("Unlinking file my_dir\\foo.txt...\n");
-	if (unlink("my_dir\\foo.txt") != 0)
+	printf("Creating file bar.txt...\n");
+	HANDLE file = CreateFile("foo.txt", GENERIC_WRITE,
+							 FILE_SHARE_DELETE, NULL,
+							 CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (file == INVALID_HANDLE_VALUE)
 	{
 		printf("Failed, error = %d, errno = %d (%s).\n", GetLastError(), errno, strerror(errno));
 		exit(1);
 	}
 
-	printf("Opening directory my_dir...\n");
-	HANDLE dir = CreateFile("my_dir", GENERIC_READ,
-							FILE_SHARE_READ | FILE_SHARE_DELETE,
-							0, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, 0);
-	if (dir == INVALID_HANDLE_VALUE)
+
+	printf("Renaming foo.txt to bar.txt...\n");
+	if (unlink("foo.txt", "bar.txt") != 0)
 	{
 		printf("Failed, error = %d, errno = %d (%s).\n", GetLastError(), errno, strerror(errno));
 		exit(1);
 	}
-
-	printf("Removing directory my_dir with rmdir()...\n");
-	if (rmdir("my_dir") != 0)
-	{
-		printf("Failed, error = %d, errno = %d (%s).\n", GetLastError(), errno, strerror(errno));
-		exit(1);
-	}
-
-	printf("Huh.\n");
 
 	return 0;
 }
