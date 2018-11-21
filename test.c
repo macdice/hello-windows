8e2fa50722908474e6ef8d94922d6b431811bf7c
Thomas Munro <thomas.munro@enterprisedb.com>
2018-11-21 17:17:24 +1300

    fixup

--- test.c
+++ test.c
@@ -32,12 +32,29 @@ main(int argc, char *argv[])
 		printf("Failed, error = %d, errno = %d (%s).\n", GetLastError(), errno, strerror(errno));
 		exit(1);
 	}
-*/
 CloseHandle(file);
 CloseHandle(file2);
+*/
 
+	/* this doesn't work */
 	printf("Renaming foo.txt to bar.txt...\n");
 	if (rename("foo.txt", "bar.txt") != 0)
+	{
+		printf("Failed, error = %d, errno = %d (%s).\n", GetLastError(), errno, strerror(errno));
+		exit(1);
+	}
+
+	/* this does work */
+	const char *new_name = "bar.txt";
+	size_t size = strlen(new_name);
+	FILE_RENAME_INFO *rename_info = malloc(sizeof(FILE_RENAME_INFO) + size);
+	rename_info->Flags = FILE_RENAME_FLAG_POSIX_SEMANTICS;
+	rename_info->ReplaceIfExists = true;
+	rename_info->RootDirectory = NULL;
+	rename_info->FileNameLength = size;
+	strcpy(rename_info->FileName, new_name);
+	printf("Performing mysterious SetFileInformationByHandle operation...\n");
+	if (SetFileInformationByHandle(file, FileRenameInfoEx, &rename_info, sizeof(FILE_RENAME_INFO) + size))
 	{
 		printf("Failed, error = %d, errno = %d (%s).\n", GetLastError(), errno, strerror(errno));
 		exit(1);
