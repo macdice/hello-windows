c365ebc2fc79f49edb1d741ba54685da6aeb76dc
Thomas Munro <thomas.munro@gmail.com>
2020-11-03 13:45:57 +1300

    fixup

--- test.c
+++ test.c
@@ -3,30 +3,39 @@
 
 #include <windows.h>
 
-int
-main(int argc, char *argv[])
+void
+doit(const char *collcollate)
 {
 	NLSVERSIONINFOEX version = {sizeof(NLSVERSIONINFOEX)};
 	WCHAR wide_collcollate[LOCALE_NAME_MAX_LENGTH];
 	size_t converted_size;
 
-	mbstowcs_s(&converted_size, wide_collcollate, LOCALE_NAME_MAX_LENGTH,
-			   "en-NZ", _TRUNCATE);
-			   //"English_United States.1252", _TRUNCATE);
-//	MultiByteToWideChar(CP_ACP, 0, collcollate, -1, wide_collcollate,
-//						LOCALE_NAME_MAX_LENGTH);
-	if (GetNLSVersionEx(COMPARE_STRING, wide_collcollate, &version))
+	printf("%s", collcollate);
+	if (mbstowcs_s(&converted_size, wide_collcollate, LOCALE_NAME_MAX_LENGTH, collcollate, _TRUNCATE) != 0)
 	{
-		printf("%d.%d,%d.%d",
+		printf(" (failed to convert to wchar)\n");
+	}
+	else if (GetNLSVersionEx(COMPARE_STRING, wide_collcollate, &version))
+	{
+		printf(" -> version %d.%d,%d.%d",
 			   (version.dwNLSVersion >> 8) & 0xFFFF,
 			   version.dwNLSVersion & 0xFF,
 			   (version.dwDefinedVersion >> 8) & 0xFFFF,
 			   version.dwDefinedVersion & 0xFF);
-		return 0;
 	}
 	else
 	{
-		fprintf(stderr, "Error %d\n", GetLastError());
-		return 1;
+		printf(" -> error %d\n", GetLastError());
 	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	doit("English_United States");
+	doit("English_United States.1252");
+	doit("English_United States.UTF-8");
+	doit("en-US");
+	doit("en-US.1252");
+	doit("en-US.UTF-8");
 }
