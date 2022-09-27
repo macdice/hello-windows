061ff2c0938e0298ac480a801a84c07e5987710c
Thomas Munro <thomas.munro@gmail.com>
2022-09-27 20:05:56 +1300

    hack

--- test.c
+++ test.c
@@ -1,67 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-static void
-doit(const char *name)
-{
-	char *full;
-
-	errno = 0;
-	full = _fullpath(NULL, name, 0);
-	if (full) {
-		printf("%s -> %s\n", name, full);
-		free(full);
-	}
-	else
-		printf("%s -> NULL, errno = %d, %s\n", name, errno, strerror(errno));
-}
+#include <windows.h>
 
 int
 main(int argc, char *argv[])
 {
-	doit("c:\\windows");
-	doit("c:\\does not exist.txt");
-	doit("x:\\bad drive.txt");
-	doit("x:bad drive.txt");
-	doit("1:\\very bad drive.txt");
-	doit("1:very bad drive.txt");
-	doit("cc:\\impossible drive.txt");
-	doit("cc:impossible drive.txt");
-	doit("a:remember floppy drives?.txt");
-	doit("test.c");
-	doit("test.c/foo");
-	doit(".");
-	doit("..");
-	doit("../..");
-	doit("../../..");
-	doit("../../../..");
-	doit("../../../../..");
-	doit("../../../../../..");
-	doit("../../../../../../..");
-	doit("../../../../../../../..");
-	doit("..\\..");
-	doit("c:\\.\\does not exists\\..\\windows");
-	doit("good_junction");
-	doit("good_junction/.");
-	doit("good_junction/..");
-	doit("good_junction/../my_junction");
-	doit("good_junction/system32");
-	doit("good_symlink");
-	doit("good_symlink/.");
-	doit("good_symlink/..");
-	doit("good_symlink/../my_symlink");
-	doit("good_symlink/system32");
-	doit("bad_junction");
-	doit("bad_junction/.");
-	doit("bad_junction/..");
-	doit("bad_junction/../my_junction");
-	doit("bad_junction/system32");
-	doit("bad_symlink");
-	doit("bad_symlink/.");
-	doit("bad_symlink/..");
-	doit("bad_symlink/../my_symlink");
-	doit("bad_symlink/system32");
-	return 0;
+	HANDLE handle;
+	OVERLAPPED overlapped;
+
+	handle = CreateFile("test.txt", GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (handle == INVALID_HANDLE_VALUE) {
+		printf("CreateFile failed\n");
+		return EXIT_FAILURE:
+	}
+
+	printf("pointer %d\n", SetFilePointer(handle, 0, NULL, FILE_CURRENT));
+	if (!WriteFile(handle, "hello world\n", 12, NULL, &overlapped)) {
+		printf("WriteFile failed\n");
+		return EXIT_FAILURE;
+	}
+	printf("pointer %d\n", SetFilePointer(handle, 0, NULL, FILE_CURRENT));
+
+	return EXIT_SUCCESS;
 }
 
