ad7ca08ca9b22f011d5931960aca7944c9ed0fc4
Thomas Munro <thomas.munro@gmail.com>
2022-09-27 23:32:11 +1300

    fixup

--- test.c
+++ test.c
@@ -30,7 +30,7 @@ main(int argc, char *argv[])
 
 	CloseHandle(handle);
 
-	handle = CreateFile("test.txt", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	handle = CreateFile("test.txt", GENERIC_WRITE | GENERIC_READ, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (handle == INVALID_HANDLE_VALUE) {
 		printf("CreateFile failed\n");
 		return EXIT_FAILURE;
