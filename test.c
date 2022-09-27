5c74fc82e0f397ae35bee6d871bd93accc71e67c
Thomas Munro <thomas.munro@gmail.com>
2022-09-27 22:14:05 +1300

    fixup

--- test.c
+++ test.c
@@ -29,8 +29,7 @@ main(int argc, char *argv[])
 
 	CloseHandle(handle);
 
-	//handle = CreateFile("test.txt", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
-	handle = CreateFile("test.txt", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_FLAG_NORMAL, NULL);
+	handle = CreateFile("test.txt", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
 	if (handle == INVALID_HANDLE_VALUE) {
 		printf("CreateFile failed\n");
 		return EXIT_FAILURE;
