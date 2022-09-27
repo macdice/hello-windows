fff0296f4cac510b920eb59c3ec11ef0b3535909
Thomas Munro <thomas.munro@gmail.com>
2022-09-27 13:33:12 +1300

    fixup

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 int
 main(int argc, char *argv[])
 {
-	if (!fullpath(NULL, "c:\\does not exists.txt", 0))
+	if (!_fullpath(NULL, "c:\\does not exists.txt", 0))
 		printf("errno = %d, %s\n", errno, strerror(errno));
 
 	return 0;
