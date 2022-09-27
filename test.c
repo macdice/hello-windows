3667aaa279bd769e3efae48c7fa16881590866a4
Thomas Munro <thomas.munro@gmail.com>
2022-09-27 20:08:02 +1300

    fixup

--- test.c
+++ test.c
@@ -12,14 +12,18 @@ main(int argc, char *argv[])
 	handle = CreateFile("test.txt", GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (handle == INVALID_HANDLE_VALUE) {
 		printf("CreateFile failed\n");
-		return EXIT_FAILURE:
+		return EXIT_FAILURE;
 	}
 
 	printf("pointer %d\n", SetFilePointer(handle, 0, NULL, FILE_CURRENT));
+
+	memset(&overlapped, 0, sizeof(overlapped));
+	overlapped.Offset = 0;
 	if (!WriteFile(handle, "hello world\n", 12, NULL, &overlapped)) {
 		printf("WriteFile failed\n");
 		return EXIT_FAILURE;
 	}
+
 	printf("pointer %d\n", SetFilePointer(handle, 0, NULL, FILE_CURRENT));
 
 	return EXIT_SUCCESS;
