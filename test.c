95ebe9ae76cc7f6ba45a213252390a48cf8bf437
Thomas Munro <thomas.munro@gmail.com>
2022-09-27 13:24:44 +1300

    fixup

--- test.c
+++ test.c
@@ -1,69 +1,14 @@
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
-#include <winsock2.h>
-#include <afunix.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#pragma comment(lib, "Ws2_32.lib")
-
-void
-doit(size_t length)
-{
-	SOCKET sock;
-	struct sockaddr_un *addr;
-	size_t addr_size;
-
-	sock = socket(AF_UNIX, SOCK_STREAM, 0);
-	if (sock == INVALID_SOCKET)
-	{
-		printf("socket() failed: %d\n", WSAGetLastError());
-		return;
-	}
-
-	addr_size = sizeof(addr->sun_family) + length + 3 + 1; /* "C:/" + NUL */
-	addr = malloc(addr_size);
-	memset(addr, 0, addr_size);
-	addr->sun_family = AF_UNIX;
-	//memcpy(&addr->sun_path, "C:/", 3);
-	memcpy(&addr->sun_path, "\000C:/", 4);
-	for (size_t i = 0; i < length; ++i)
-		addr->sun_path[i + 4] = 'x';
-
-	/* make the prefix distinct to find out if error 10048 is because name is chomped at 240...*/
-	//if (length > 10)
-//		sprintf(&addr->sun_path[4], "%zu", length);
-
-	printf("Can I make a socket with path length %zu? ", length); //addr->sun_path);
-
-	if (bind(sock, (struct sockaddr *) addr, 4 + length + sizeof(addr->sun_family)) == SOCKET_ERROR)
-	{
-		printf("bind() failed: %d\n", WSAGetLastError());
-		return;
-	}
-	printf("yes!\n");
-	//unlink(addr->sun_path); // leave it behind in the file system
-	free(addr);
-	//closesocket(sock);
-}
 
 int
 main(int argc, char *argv[])
 {
-	WSADATA wsadata;
-	int rc;
+	char *x = fullpath(NULL, "c:\\does not exists.txt", 0);
 
-	rc = WSAStartup(MAKEWORD(2, 2), &wsadata);
-	if (rc != 0)
-	{
-		printf("WSAStartup failed: %d\n", rc);
-		return;
-	}
+	if (!x) {
+		perror("_fullpath");
 
-	doit(10);
-	doit(10);	/* verify that duplicate name fails */
-	for (int i = 100; i < 110; i++)
-		doit(i);
+	return 0;
 }
 
