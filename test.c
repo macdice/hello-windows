53a36ad3f56a029c32881bd7f204b4e0403f0fd6
Thomas Munro <thomas.munro@gmail.com>
2024-08-19 16:21:41 +1200

    fixup

--- test.c
+++ test.c
@@ -6,14 +6,41 @@
 
 DWORD WINAPI f(LPVOID p)
 {
+	printf("thread %d: default localeconv() = %p\n",
+		(int) GetCurrentThreadId(),
+		localeconv());
+
+	setlocale(LC_ALL, "es-ES");
+
+	printf("thread %d: es-ES localeconv() = %p\n",
+		(int) GetCurrentThreadId(),
+		localeconv());
+
 	_configthreadlocale(_ENABLE_PER_THREAD_LOCALE);
-	for (int i = 0; i < 10000000; ++i)
+
+	printf("thread %d: es-ES localeconv() in thread-local mode = %p\n",
+		(int) GetCurrentThreadId(),
+		localeconv());
+
+	setlocale(LC_ALL, "de-DE");
+
+	printf("thread %d: de-DE localeconv() in thread-local mode = %p\n",
+		(int) GetCurrentThreadId(),
+		localeconv());
+
+	printf("thread %d: starting torture test...\n",
+		(int) GetCurrentThreadId());
+
+	for (int i = 0; i < 1000000; ++i)
 	{
 		setlocale(LC_ALL, "fr-FR");
 		assert(strcmp(localeconv()->decimal_point, ",") == 0);
 		setlocale(LC_ALL, "en-GB");
 		assert(strcmp(localeconv()->decimal_point, ".") == 0);
 	}
+
+	printf("thread %d: done\n");
+
 	return 0;
 }
 
