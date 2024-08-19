5b31994fd397502818f5bc81371c519fd626c2b8
Thomas Munro <thomas.munro@gmail.com>
2024-08-19 16:14:02 +1200

    fixup

--- test.c
+++ test.c
@@ -5,21 +5,34 @@
 
 DWORD WINAPI f(LPVOID p)
 {
-	printf("localeconv() = %p\n", localeconv());
+	_configthreadlocale(_ENABLE_PER_THREAD_LOCALE);
+	for (int i = 0; i < 10000000; ++i)
+	{
+		setlocale(LC_ALL, "fr-FR");
+		assert(strcmp(localeconv()->decimal_point, ",") == 0);
+		setlocale(LC_ALL, "en-GB");
+		assert(strcmp(localeconv()->decimal_point, ".") == 0);
+	}
 	return 0;
 }
+
+#define NTHREADS 8
 
 int
 main(int argc, char *argv[])
 {
-	HANDLE threads[2];
-	DWORD thread_ids[2];
+
+	HANDLE threads[NTHREADS];
+	DWORD thread_ids[NTHREADS];
 	
-	threads[0] = CreateThread(NULL, 0, f, NULL, 0, &thread_ids[0]);
-	threads[1] = CreateThread(NULL, 0, f, NULL, 0, &thread_ids[1]);
-	WaitForMultipleObjects(2, threads, TRUE, INFINITE);
-	CloseHandle(threads[0]);
-	CloseHandle(threads[1]);
+	for (int i = 0; i < NTHREADS; ++i)
+		threads[i] = CreateThread(NULL, 0, f, NULL, 0, &thread_ids[i]);
+	
+	WaitForMultipleObjects(NTHREADS, threads, TRUE, INFINITE);
+
+	for (int i = 0; i < NTHREADS; ++i)
+		CloseHandle(threads[i]);
+
 	return EXIT_SUCCESS;
 }
 
