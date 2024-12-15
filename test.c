c9a8400b797a564e512fa7e62b152192b8e1192a
Thomas Munro <thomas.munro@gmail.com>
2024-12-16 05:05:33 +1300

    fixup

--- test.c
+++ test.c
@@ -20,21 +20,13 @@ main(int argc, char *argv[])
 		wchar_t *command_line_w;
 		wchar_t **argv_w;
 		int argc_w;
-		char **argv_a;
-		int argc_a;
 
 		command_line_a = GetCommandLineA();
 		command_line_w = GetCommandLineW();
-		printf("GetCommandLineA() = [%s]\n", command_line_a);
-		wprintf(L"GetCommandLineW() = [%s]\n", command_line_w);
-
-		argv_a = CommandLineToArgvA(command_line_a, &argc_a);
 		argv_w = CommandLineToArgvW(command_line_w, &argc_w);
 
-		printf("CommandLineToArgvA =");
-		for (int i = 0; i < argc_a; ++i)
-			printf(" [%s]", argv_a[i]);
-		printf("\n");
+		printf("GetCommandLineA() = [%s]\n", command_line_a);
+		wprintf(L"GetCommandLineW() = [%s]\n", command_line_w);
 		printf("CommandLineToArgvW =");
 		for (int i = 0; i < argc_w; ++i)
 			wprintf(L" [%s]", argv_w[i]);
