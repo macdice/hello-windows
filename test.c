683835bff0667377aeab773075d7e5746b0657dd
Thomas Munro <thomas.munro@gmail.com>
2025-07-10 13:47:10 +1200

    fixup

--- test.c
+++ test.c
@@ -3,12 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const char *or_null(const char *s)
+{
+	return s == NULL ? "<null>" : s;
+}
+
 int
 main(int argc, char *argv[])
 {
 	if (setlocale(LC_ALL, "English, United Kingdom") == NULL)
 		printf("error 1\n");
-	printf("got: %s\n", setlocale(LC_ALL, NULL));
+	printf("got: %s\n", or_null(setlocale(LC_ALL, NULL)));
+	if (setlocale(LC_ALL, "English_United Kingdom") == NULL)
+		printf("error 2\n");
+	printf("got: %s\n", or_null(setlocale(LC_ALL, NULL)));
+	if (setlocale(LC_ALL, "English_United Kingdom.1252") == NULL)
+		printf("error 3\n");
+	printf("got: %s\n", or_null(setlocale(LC_ALL, NULL)));
 
 	return EXIT_SUCCESS;
 }
