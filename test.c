aff4c5087f2c04b5fc4aa98b42f3f42c831c9230
Thomas Munro <thomas.munro@gmail.com>
2025-11-19 13:06:45 +1300

    XXX let's see what VS2019 prints

--- test.c
+++ test.c
@@ -1,10 +1,9 @@
-#include <locale.h>
+#include <inttypes.h>
 #include <stdio.h>
 
 int
 main()
 {
-	_locale_t loc1 = _create_locale(LC_ALL, "C");
-	_locale_t loc2 = _create_locale(LC_ALL, "C");
-	printf("%p %p\n", loc1, loc2);
+	puts(PRId64);
+	puts("\n");
 }
